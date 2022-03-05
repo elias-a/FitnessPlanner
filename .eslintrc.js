64003bf2a4e810aa93b78321628c98ed7bbdd222
elias-a <elias.alstead@gmail.com>
2022-03-05 11:26:40 -0500

    Add initial app structure and config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'react-native/no-inline-styles': 0,
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
+};
---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+/*import {
+
+} from 'react-native';*/
+
+const App = () => {
+  return <></>;
+};
+
+/*const styles = StyleSheet.create({
+
+});*/
+
+export default App;
