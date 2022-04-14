9f192c1c41a89d394e21362642009d31c147fdc0
elias-a <elias.alstead@gmail.com>
2022-04-14 18:46:11 -0400

    Set dates to the same time of day before computing the difference

--- src/utils/getDayKey.ts
+++ src/utils/getDayKey.ts
@@ -1,10 +1,13 @@
-export const getDayKey = (date1: Date, date2: Date) => {
+export const getDayKey = (dateTime1: Date, dateTime2: Date) => {
+  const date1 = new Date(dateTime1);
+  const date2 = new Date(dateTime2);
+  date1.setUTCHours(0, 0, 0, 0);
+  date2.setUTCHours(0, 0, 0, 0);
+
   const dayDifference = Math.ceil(
     (date1.getTime() - date2.getTime()) / 1000 / 86400,
   );
 
-  let day = dayDifference % 7;
-  day = day === 0 ? 7 : day;
-
+  const day = (dayDifference % 7) + 1;
   return day.toString();
 };
