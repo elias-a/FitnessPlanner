861547439f29ddb89656bbc47106481bd13a2b8d
elias-a <elias.alstead@gmail.com>
2022-05-07 23:06:52 -0400

    Move functionality for creating/editing exercises to a modal

--- src/components/Modals/Exercise.tsx
+++ src/components/Modals/Exercise.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text, Pressable, TextInput, StyleSheet } from 'react-native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Modal from './Modal';
-import MultiSelect from '../MultiSelect';
+import SelectDropdown from 'react-native-select-dropdown';
 import { useAppSelector } from '../../hooks';
 import type { Exercise } from '../../types/exercise';
+import Category from '../../models/schema/category';
 
 const initialExercise: Exercise = {
   id: '',
@@ -35,12 +36,18 @@ const ExerciseModal: React.FC<ExerciseModalProps> = ({
     }));
   };
 
+  const handleCancel = () => {
+    setExercise(initialExercise);
+    onCancel();
+  };
+
   const handleSave = () => {
     onSave(exercise, editing);
+    setExercise(initialExercise);
   };
 
   return (
-    <Modal isOpen={isOpen} close={onCancel}>
+    <Modal isOpen={isOpen} close={handleCancel}>
       <View style={styles.modal}>
         <View style={styles.container}>
           <View style={styles.textInputSection}>
@@ -53,15 +60,29 @@ const ExerciseModal: React.FC<ExerciseModalProps> = ({
           </View>
 
           <View style={styles.multiSelectSection}>
-            <MultiSelect
-              items={categories}
-              selectedItems={[]}
-              onSelectedItemsChange={items =>
-                updateExercise('categories', items)
+            <SelectDropdown
+              data={categories}
+              defaultButtonText={'Select category...'}
+              buttonTextAfterSelection={item => item.name}
+              rowTextForSelection={item => item.name}
+              onSelect={(selectedCategory: Category) =>
+                updateExercise('categories', [selectedCategory.id])
               }
-              isSingle={false}
-              subKey={'subCategories'}
-              selectText={'Choose categories...'}
+              buttonStyle={styles.dropdown1BtnStyle}
+              buttonTextStyle={styles.dropdown1BtnTxtStyle}
+              renderDropdownIcon={isOpened => {
+                return (
+                  <FontAwesome
+                    name={isOpened ? 'chevron-up' : 'chevron-down'}
+                    color={'#444'}
+                    size={18}
+                  />
+                );
+              }}
+              dropdownIconPosition={'right'}
+              dropdownStyle={styles.dropdown1DropdownStyle}
+              rowStyle={styles.dropdown1RowStyle}
+              rowTextStyle={styles.dropdown1RowTxtStyle}
             />
           </View>
 
@@ -80,8 +101,8 @@ const styles = StyleSheet.create({
   modal: {
     position: 'absolute',
     bottom: 0,
-    minHeight: '70%',
-    maxHeight: '70%',
+    minHeight: '60%',
+    maxHeight: '60%',
     minWidth: '100%',
     maxWidth: '100%',
     backgroundColor: '#fff',
@@ -94,6 +115,9 @@ const styles = StyleSheet.create({
   },
   textInputSection: {
     flex: 1,
+    paddingTop: 30,
+    minHeight: 70,
+    maxHeight: 70,
   },
   textInput: {
     flex: 1,
@@ -106,6 +130,9 @@ const styles = StyleSheet.create({
   },
   multiSelectSection: {
     flex: 2,
+    paddingTop: 30,
+    minHeight: 70,
+    maxHeight: 70,
   },
   saveButtonSection: {
     flex: 3,
@@ -114,7 +141,8 @@ const styles = StyleSheet.create({
     paddingVertical: 30,
   },
   addButton: {
-    flex: 1,
+    flex: 2,
+    alignSelf: 'flex-end',
     minWidth: 350,
     maxWidth: 350,
     minHeight: 50,
@@ -122,6 +150,33 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#909090',
+  },
+
+  dropdown1BtnStyle: {
+    padding: 0,
+    margin: 0,
+    minWidth: 300,
+    maxWidth: 300,
+    minHeight: 40,
+    maxHeight: 40,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#444',
+  },
+  dropdown1BtnTxtStyle: {
+    color: '#444',
+    textAlign: 'left',
+  },
+  dropdown1DropdownStyle: {
+    backgroundColor: '#efefef',
+  },
+  dropdown1RowStyle: {
+    backgroundColor: '#efefef',
+    borderBottomColor: '#c5c5c5',
+  },
+  dropdown1RowTxtStyle: {
+    color: '#444',
+    textAlign: 'left',
   },
 });
 
