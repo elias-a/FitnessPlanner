46896bc07224d974f784751292897821358d0344
elias-a <elias.alstead@gmail.com>
2022-05-18 21:03:24 -0400

    Fix bug with determining the number of days between two dates

--- src/utils/getDayKey.ts
+++ src/utils/getDayKey.ts
@@ -1,8 +1,8 @@
 export const getDayKey = (dateTime1: Date, dateTime2: Date) => {
   const date1 = new Date(dateTime1);
   const date2 = new Date(dateTime2);
-  date1.setUTCHours(0, 0, 0, 0);
-  date2.setUTCHours(0, 0, 0, 0);
+  date1.setHours(0, 0, 0, 0);
+  date2.setHours(0, 0, 0, 0);
 
   const dayDifference = Math.ceil(
     (date1.getTime() - date2.getTime()) / 1000 / 86400,
