9e8a5dc935bc77920a96d3a256f4da49de3e2f9b
elias-a <elias.alstead@gmail.com>
2022-07-23 13:14:10 -0400

    Create navigation component for the Glossary view

--- src/components/Glossary/index.tsx
+++ src/components/Glossary/index.tsx
@@ -1,52 +1,8 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Landing from './Landing';
-import ViewExercises from './views/ViewExercises';
-import ViewCategories from './views/ViewCategories';
-import ViewSplits from './views/ViewSplits';
-
-export type Stack = {
-  Landing: undefined;
-  ViewExercises: undefined;
-  ViewCategories: undefined;
-  ViewSplits: undefined;
-};
-
-const Stack = createNativeStackNavigator();
+import Navigation from './Navigation';
 
 const Glossary: React.FC<{}> = ({}) => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Landing"
-        component={Landing}
-        options={{
-          header: () => null,
-        }}
-      />
-      <Stack.Screen
-        name="ViewExercises"
-        component={ViewExercises}
-        options={{
-          header: () => null,
-        }}
-      />
-      <Stack.Screen
-        name="ViewCategories"
-        component={ViewCategories}
-        options={{
-          header: () => null,
-        }}
-      />
-      <Stack.Screen
-        name="ViewSplits"
-        component={ViewSplits}
-        options={{
-          header: () => null,
-        }}
-      />
-    </Stack.Navigator>
-  );
+  return <Navigation />;
 };
 
 export default Glossary;
